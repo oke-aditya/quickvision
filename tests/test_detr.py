750295599f31bdd8b612b39d5497e0c3ad2788a0
Aditya Oke <okeaditya315@gmail.com>
2020-11-07 22:41:51 +0530

    sketch detr tests

---
+++ tests/test_detr.py
@@ -0,0 +1,100 @@
+import unittest
+import torch
+from typing import Dict
+from PIL import Image
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch_utils import im2tensor
+from vision.models.detection import detr
+from vision.models import model_utils
+from vision.models.detection.detr import engine
+from dataset_utils import DummyDetectionDataset
+
+if(torch.cuda.is_available()):
+    from torch.cuda import amp
+
+train_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=10)
+val_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=10)
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2,
+                                           shuffle=False, collate_fn=collate_fn)
+
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=2,
+                                         shuffle=False, collate_fn=collate_fn)
+
+
+class ModelFactoryTester(unittest.TestCase):
+    def test_detr_backbones(self):
+        pass
+
+
+class EngineTester(unittest.TestCase):
+    def test_train(self):
+        # Read Image using PIL Here
+        # Do forward over image
+        image = Image.open("tests/assets/grace_hopper_517x606.jpg")
+        img_tensor = im2tensor(image)
+        self.assertEqual(img_tensor.ndim, 4)
+        boxes = torch.tensor([[0, 0, 100, 100], [0, 1, 2, 2],
+                             [10, 15, 30, 35], [23, 35, 93, 95]], dtype=torch.float)
+        labels = torch.tensor([1, 2, 3, 4], dtype=torch.int64)
+        targets = [{"boxes": boxes, "labels": labels}]
+        return True
+
+    def test_infer(self):
+        # Infer over an image
+        image = Image.open("tests/assets/grace_hopper_517x606.jpg")
+        tensor = im2tensor(image)
+        self.assertEqual(tensor.ndim, 4)
+        return True
+
+    def test_train_step(self):
+        pass
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
+    def test_train_step_cuda(self):
+        pass
+
+    def test_val_step(self):
+        pass
+
+    def test_fit(self):
+        pass
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
+    def test_fit_cuda(self):
+        pass
+
+    def test_train_sanity_fit(self):
+        pass
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
+    def test_train_sanity_fit_cuda(self):
+        pass
+
+    def test_val_sanity_fit(self):
+        pass
+
+    def test_sanity_fit(self):
+        pass
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
+    def test_sanity_fit_cuda(self):
+        pass
+
+
+class LightningTester(unittest.TestCase):
+    def test_lit_detr(self):
+        pass
+
+    @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
+    def test_lit_detr_cuda(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
