b584d1bc2e0702f75cc3f13e245b5a2e4f9c2005
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-11-07 15:03:14 +0530

    Completes FRCNN (#9)
    * Adds fit method
    
    * skip the test, lower CI
    
    * fix CI
    
    * Small bug fixes
    
    * fix pep8

--- tests/test_frcnn.py
+++ tests/test_frcnn.py
@@ -20,8 +20,8 @@ non_fpn_supported_models = ["mobilenet"]
 # "resnet18", "resnet34", "resnet50","resnet101",
 # "resnet152", "resnext101_32x8d", "mobilenet", "vgg11", "vgg13", "vgg16", "vgg19"
 
-train_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=100)
-val_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=100)
+train_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=10)
+val_dataset = DummyDetectionDataset(img_shape=(3, 256, 256), num_samples=10)
 
 
 def collate_fn(batch):
@@ -93,12 +93,12 @@ class EngineTester(unittest.TestCase):
             frcnn_model = faster_rcnn.create_vision_fastercnn(num_classes=3, backbone=backbone)
             self.assertTrue(isinstance(frcnn_model, nn.Module))
             opt = torch.optim.Adam(frcnn_model.parameters(), lr=1e-3)
-            out = faster_rcnn.train_step(frcnn_model, train_loader, "cpu", opt, num_batches=10)
-            self.assertIsInstance(out, Dict)
-            self.assertIsInstance(out["loss_classifier"], torch.Tensor)
-            self.assertIsInstance(out["loss_box_reg"], torch.Tensor)
-            self.assertIsInstance(out["loss_objectness"], torch.Tensor)
-            self.assertIsInstance(out["loss_rpn_box_reg"], torch.Tensor)
+            met = faster_rcnn.train_step(frcnn_model, train_loader, "cpu", opt, num_batches=10)
+            self.assertIsInstance(met, Dict)
+            self.assertIsInstance(met["loss_classifier"], torch.Tensor)
+            self.assertIsInstance(met["loss_box_reg"], torch.Tensor)
+            self.assertIsInstance(met["loss_objectness"], torch.Tensor)
+            self.assertIsInstance(met["loss_rpn_box_reg"], torch.Tensor)
 
     @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
     def test_train_step_fpn_cuda(self):
@@ -128,11 +128,45 @@ class EngineTester(unittest.TestCase):
             self.assertIsInstance(metrics["giou"], torch.Tensor)
 
     def test_fit(self):
-        pass
+        for bbone in fpn_supported_models:
+            backbone = faster_rcnn.create_fastercnn_backbone(name=bbone, pretrained=False)
+            self.assertTrue(isinstance(backbone, nn.Module))
+            frcnn_model = faster_rcnn.create_vision_fastercnn(num_classes=3, backbone=backbone)
+            self.assertTrue(isinstance(frcnn_model, nn.Module))
+            opt = torch.optim.Adam(frcnn_model.parameters(), lr=1e-3)
+            history = faster_rcnn.fit(frcnn_model, 1, train_loader, val_loader, "cpu", opt, num_batches=10)
+
+            self.assertIsInstance(history, Dict)
+            exp_keys = ("train", "val")
+            for exp_k in exp_keys:
+                self.assertTrue(exp_k in history.keys())
+            # exp_keys2 = ("train_loss")
+            # for exp_k2 in exp_keys2:
+            #     self.assertTrue(exp_k2 in history["train"].keys())
+            # exp_keys3 = ("val_iou", "val_giou")
+            # for exp_k3 in exp_keys3:
+            #     self.assertTrue(exp_k3 in history["val"].keys())
 
     @unittest.skipIf(not torch.cuda.is_available(), "CUDA unavailable")
     def test_fit_cuda(self):
-        pass
+        for bbone in fpn_supported_models:
+            backbone = faster_rcnn.create_fastercnn_backbone(name=bbone, pretrained=False)
+            self.assertTrue(isinstance(backbone, nn.Module))
+            frcnn_model = faster_rcnn.create_vision_fastercnn(num_classes=3, backbone=backbone)
+            self.assertTrue(isinstance(frcnn_model, nn.Module))
+            opt = torch.optim.Adam(frcnn_model.parameters(), lr=1e-3)
+            history = faster_rcnn.fit(frcnn_model, 1, train_loader, val_loader, "cuda", opt, num_batches=10, fp16=True)
+
+            self.assertIsInstance(history, Dict)
+            exp_keys = ("train", "val")
+            for exp_k in exp_keys:
+                self.assertTrue(exp_k in history.keys())
+            # exp_keys2 = ("train_loss")
+            # for exp_k2 in exp_keys2:
+            #     self.assertTrue(exp_k2 in history["train"].keys())
+            # exp_keys3 = ("val_iou", "val_giou")
+            # for exp_k3 in exp_keys3:
+            #     self.assertTrue(exp_k3 in history["val"].keys())
 
     def test_train_sanity_fit(self):
         pass
@@ -171,6 +205,7 @@ class LightningTester(unittest.TestCase):
             trainer.fit(model, train_loader, val_loader)
         flag = True
         self.assertTrue(flag)
+
 
 if __name__ == '__main__':
     unittest.main()
