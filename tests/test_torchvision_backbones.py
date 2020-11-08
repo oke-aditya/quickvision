ae8cb6a52aa108cf10caa812f3e35ca7868129e8
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-11-08 16:05:42 +0530

    Deprecate old tests, use more backbone, use mobilenetv2 (#13)
    * deprecate old tests, use more backbone, use mobilenetv2
    
    * false pretrained, adds more bbones
    
    * Adds one more test

--- tests/test_torchvision_backbones.py
+++ tests/test_torchvision_backbones.py
@@ -3,14 +3,19 @@ import torch
 import torch.nn as nn
 from vision.models.components.torchvision_backbones import create_torchvision_backbone
 
-supported_tv_models = ["resnet18", "vgg13", "resnet50", "mobilenet", "mnasnet0_5", ]
+supported_tv_models = ["vgg11", "vgg13", "vgg16", "vgg19", "resnet18",
+                       "resnet34", "resnet50", "resnet101", "resnet152",
+                       "resnext50_32x4d", "resnext101_32x8d",
+                       "mnasnet0_5", "mnasnet0_75", "mnasnet1_0", "mnasnet1_3",
+                       "mobilenet_v2"]
+
 error_model = "invalid_model"
 
 
 class BackboneTester(unittest.TestCase):
     def test_torchvision_backbones(self):
         for model_name in supported_tv_models:
-            ft_backbone, out_channels = create_torchvision_backbone(model_name)
+            ft_backbone, out_channels = create_torchvision_backbone(model_name, pretrained=False)
             self.assertTrue(isinstance(ft_backbone, nn.Module))
             self.assertTrue(isinstance(out_channels, int))
 
