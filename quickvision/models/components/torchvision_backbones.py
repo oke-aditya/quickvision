2db89af5e65d41a0fad2c432735ec8fbaf5c6dcf
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-12-02 19:32:56 +0530

    Cleans up code like bolts (#65)
    * restructures code like bolts
    
    * fixes small warning to docs
    
    * one more try for docs
    
    * fixes model weights

--- quickvision/models/components/torchvision_backbones.py
+++ quickvision/models/components/torchvision_backbones.py
@@ -1,8 +1,9 @@
 
 import torch.nn as nn
 import torchvision
+from quickvision.pretrained._model_zoo import TORCHVISION_MODEL_ZOO
 from quickvision.pretrained._pretrained_weights import _load_pretrained_weights
-from quickvision.pretrained._pretrained_cnns import weights_dict
+from quickvision.pretrained._pretrained_cnns import WEIGHTS_DICT
 
 __all__ = ["create_torchvision_backbone"]
 
@@ -57,9 +58,9 @@ def create_torchvision_backbone(model_name: str, pretrained: str = None):
     """
 
     if model_name == "mobilenet_v2":
-        net = torchvision.models.mobilenet_v2(pretrained=False)
+        net = TORCHVISION_MODEL_ZOO[model_name]
         if pretrained is not None:
-            state_dict = _load_pretrained_weights(weights_dict, model_name, pretrained=pretrained)
+            state_dict = _load_pretrained_weights(WEIGHTS_DICT, model_name, pretrained=pretrained)
             net.load_state_dict(state_dict)
 
         out_channels = 1280
@@ -68,17 +69,10 @@ def create_torchvision_backbone(model_name: str, pretrained: str = None):
 
     elif model_name in ["vgg11", "vgg13", "vgg16", "vgg19", ]:
         out_channels = 512
-        if model_name == "vgg11":
-            net = torchvision.models.vgg11(pretrained=False)
-        elif model_name == "vgg13":
-            net = torchvision.models.vgg13(pretrained=False)
-        elif model_name == "vgg16":
-            net = torchvision.models.vgg16(pretrained=False)
-        elif model_name == "vgg19":
-            net = torchvision.models.vgg19(pretrained=False)
+        net = TORCHVISION_MODEL_ZOO[model_name]
 
         if pretrained is not None:
-            state_dict = _load_pretrained_weights(weights_dict, model_name, pretrained=pretrained)
+            state_dict = _load_pretrained_weights(WEIGHTS_DICT, model_name, pretrained=pretrained)
             net.load_state_dict(state_dict)
 
         ft_backbone = _create_backbone_features(net, out_channels)
@@ -86,13 +80,10 @@ def create_torchvision_backbone(model_name: str, pretrained: str = None):
 
     elif model_name in ["resnet18", "resnet34"]:
         out_channels = 512
-        if model_name == "resnet18":
-            net = torchvision.models.resnet18(pretrained=False)
-        elif model_name == "resnet34":
-            net = torchvision.models.resnet34(pretrained=False)
+        net = TORCHVISION_MODEL_ZOO[model_name]
 
         if pretrained is not None:
-            state_dict = _load_pretrained_weights(weights_dict, model_name, pretrained=pretrained)
+            state_dict = _load_pretrained_weights(WEIGHTS_DICT, model_name, pretrained=pretrained)
             net.load_state_dict(state_dict)
 
         ft_backbone = _create_backbone_adaptive(net, out_channels)
@@ -100,19 +91,10 @@ def create_torchvision_backbone(model_name: str, pretrained: str = None):
 
     elif model_name in ["resnet50", "resnet101", "resnet152", "resnext50_32x4d", "resnext101_32x8d", ]:
         out_channels = 2048
-        if model_name == "resnet50":
-            net = torchvision.models.resnet50(pretrained=False)
-        elif model_name == "resnet101":
-            net = torchvision.models.resnet101(pretrained=False)
-        elif model_name == "resnet152":
-            net = torchvision.models.resnet152(pretrained=False)
-        elif model_name == "resnext50_32x4d":
-            net = torchvision.models.resnext50_32x4d(pretrained=False)
-        elif model_name == "resnext101_32x8d":
-            net = torchvision.models.resnext101_32x8d(pretrained=False)
+        net = TORCHVISION_MODEL_ZOO[model_name]
 
         if pretrained is not None:
-            state_dict = _load_pretrained_weights(weights_dict, model_name, pretrained=pretrained)
+            state_dict = _load_pretrained_weights(WEIGHTS_DICT, model_name, pretrained=pretrained)
             net.load_state_dict(state_dict)
 
         ft_backbone = _create_backbone_adaptive(net, 2048)
@@ -120,21 +102,14 @@ def create_torchvision_backbone(model_name: str, pretrained: str = None):
 
     elif model_name in ["mnasnet0_5", "mnasnet0_75", "mnasnet1_0", "mnasnet1_3"]:
         out_channels = 1280
-        if model_name == "mnasnet0_5":
-            net = torchvision.models.mnasnet0_5(pretrained=False)
-        elif model_name == "mnasnet0_75":
-            net = torchvision.models.mnasnet0_75(pretrained=False)
-        elif model_name == "mnasnet1_0":
-            net = torchvision.models.mnasnet1_0(pretrained=False)
-        elif model_name == "mnasnet1_3":
-            net = torchvision.models.mnasnet1_3(pretrained=False)
+        net = TORCHVISION_MODEL_ZOO[model_name]
 
         if pretrained is not None:
-            state_dict = _load_pretrained_weights(weights_dict, model_name, pretrained=pretrained)
+            state_dict = _load_pretrained_weights(WEIGHTS_DICT, model_name, pretrained=pretrained)
             net.load_state_dict(state_dict)
 
         ft_backbone = _create_backbone_adaptive(net, 1280)
         return ft_backbone, out_channels
 
     else:
-        raise ValueError("Unsupported model")
+        raise ValueError(f"Unsupported model: '{model_name}'")
