30ba6320cc60c9b41607067d9cac74029f4b160c
Aditya Oke <okeaditya315@gmail.com>
2020-12-11 00:49:25 +0530

    adds more protoype code

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -7,10 +7,20 @@ from quickvision.losses import functional as fl
 
 class DiceLossTester(unittest.TestCase):
     def test_dice_loss_functional():
+        # Same thing what you do with below.
         pass
 
     def test_dice_loss():
-        pass
+        loss_fn = losses.DiceLoss()
+        inputs = torch.tensor([[0.4, 0.2, 0.3], [0.6, 0.2, 0.3]], dtype=torch.float32)
+        targets = torch.tensor([[0], [1]], dtype=torch.float32)
+        loss = loss_fn(inputs, targets)
+        # See what expected loss should be
+        expected_loss = []
+        # Compare those two with epsilon
+        loss.backward()
+        # Assert those with epsilon case
+        # self.assertTrue()
 
 
 if __name__ == "__main__":
