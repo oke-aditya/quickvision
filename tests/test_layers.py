372483ba2a1b7f3709d452c04c59f447310df163
Aditya Oke <okeaditya315@gmail.com>
2020-12-11 00:07:26 +0530

    starts adding tests

---
+++ tests/test_layers.py
@@ -0,0 +1,18 @@
+import torch
+import unittest
+import torch.nn as nn
+from quickvision import layers
+
+
+class MishTester(unittest.TestCase):
+    def test_mish():
+        pass
+
+
+class MLPTester(unittest.TestCase):
+    def test_mlp():
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_losses.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import unittest
+from quickvision import losses
+from quickvision.losses import functional as fl
+
+
+class DiceLossTester(unittest.TestCase):
+    def test_dice_loss_functional():
+        pass
+
+    def test_dice_loss():
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
