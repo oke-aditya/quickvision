34aa533696de32e9b6510ec18a3d85b14edc8adc
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2021-04-06 15:31:59 +0530

    rc1 for 0.2.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def load_long_description():
 # https://packaging.python.org/discussions/install-requires-vs-requirements /
 setup(
     name='quickvision',
-    version="0.2.0",
+    version="0.2.1rc1img",
     description="Computer Vision models and training",
     author="Aditya Oke",
     author_email="okeaditya315@gmail.com",
